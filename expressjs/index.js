fe18627cf3943ce7b7a51fa4f919e543a24d56e9
MCheryabushkin <ch.maks@list.ru>
2020-02-15 19:19:54 +0300

    Сессия в БД

--- expressjs/index.js
+++ expressjs/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
@@ -12,34 +13,29 @@ const authRoutes = require('./routes/auth');
 const User = require('./models/user');
 const varMiddleware = require('./middleware/vaiables');
 
+const MONGODB_URI = 'mongodb+srv://maksim:jXkdW12nbWf6Uycq@cluster0-umekr.mongodb.net/shop?retryWrites=true&w=majority'
 const app = express();
-
-
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MONGODB_URI
+})
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('5e3f3ddb3c5d3a08d89c445f'); 
-        req.user = user;
-        next();
-    } catch (e) {
-        console.log(e);
-    }
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(session({
     secret: 'some secret key',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store
 }));
 app.use(varMiddleware);
 
@@ -56,22 +52,14 @@ const PORT = process.env.PORT || 3000;
 
 async function start() {
     try {
-        const connectString = 'mongodb+srv://maksim:jXkdW12nbWf6Uycq@cluster0-umekr.mongodb.net/shop?retryWrites=true&w=majority'
-        await mongoose.connect(connectString, {
+        
+        await mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true,
             useFindAndModify: false,
             useUnifiedTopology: true
         });
 
-        const candidate = await User.findOne();
-        if (!candidate) {
-            const user = new User({
-                email: 'maksim@mail.ru',
-                name: 'Maksim',
-                cart: {items: []}
-            });
-            await user.save();
-        }
+       
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
--- expressjs/routes/auth.js
+++ expressjs/routes/auth.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const router = Router();
+const User = require('../models/user');
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
@@ -16,8 +17,15 @@ router.get('/logout', async (req, res) => {
 
 
 router.post('/login', async (req, res) => {
+    const user = await User.findById('5e3f3ddb3c5d3a08d89c445f'); 
+    req.session.user = user;
     req.session.isAuthenticated = true;
-    res.redirect('/');
+    req.session.save(err => {
+        if (err) {
+            throw err;
+        }
+        res.redirect('/');
+    })
 })
 
 module.exports = router;
