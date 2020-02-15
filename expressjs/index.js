986d863456ba0846f8782d54c939455c90c59748
MCheryabushkin <ch.maks@list.ru>
2020-02-15 18:50:05 +0300

    Добавление и сохранение сессии

--- expressjs/index.js
+++ expressjs/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
@@ -9,6 +10,7 @@ const cardRoutes = require('./routes/card');
 const orderRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
 const User = require('./models/user');
+const varMiddleware = require('./middleware/vaiables');
 
 const app = express();
 
@@ -34,6 +36,12 @@ app.use(async (req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: 'some secret key',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(varMiddleware);
 
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
